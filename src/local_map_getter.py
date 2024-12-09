c8828893fef7083b0a1fe254df557c483c139257
debaumann <debaumann@student.ethz.ch>
2024-12-09 16:54:24 +0000

    added my branch, got node distance, orientation detection, projects tag associated path into the duckie frame. changed package name from quack-norris to to quack_norris, - is not really used in ros and resulted in build issue, added tag info custom message. will refactor and clean up tonight

---
+++ src/local_map_getter.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+import numpy as np
+from apriltag_ros.msg import AprilTagDetectionArray
+from sensor_msgs.msg import CompressedImage, Image
+from geometry_msgs.msg import PoseStamped
+from duckietown_msgs.msg import WheelsCmdStamped
+from std_msgs.msg import Int32MultiArray
+from sensor_msgs.msg import CameraInfo
+from pyquaternion import Quaternion as PyQuaternion
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Point, Quaternion, Vector3
+from cv_bridge import CvBridge
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from quack_norris.msg import TagInfo
+import os
+import yaml
+
+
+class LocalMapNode:
+    def __init__(self):
+        rospy.init_node('local_map_node')
+        
+        self.load_parameters()
+        self.bridge = CvBridge()
+        self.bot_name = os.environ.get("VEHICLE_NAME")
+
+        self.camera_matrix = None
+        self.dist_coeffs = None
+        self.new_camera_matrix = None
+        self.map1 = None
+        self.map2 = None
+
+        self.yaml_file = rospy.get_param('~yaml_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltags.yaml')
+
+        self.forward_speed = self.drive_conroller_config['drive_params']['forward_speed']
+        self.radius = self.drive_conroller_config['drive_params']['radius']
+        self.wheel_distance = self.drive_conroller_config['drive_params']['wheel_distance']
+        
+        # Subscribers
+        self.image_sub = rospy.Subscriber(f'/{self.bot_name}/camera_node/image/compressed', CompressedImage, self.image_callback)
+        self.calibration_sub = rospy.Subscriber(f'/{self.bot_name}/camera_node/camera_info', CameraInfo, self.calib_callback)
+        self.tag_sub = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self.tag_callback)
+
+        # Publishers
+        self.pub_wheels = rospy.Publisher(f'/{self.bot_name}/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
+        self.rect_pub = rospy.Publisher(f'/{self.bot_name}/camera_node/rect', Image, queue_size=1)
+        self.tag_ids_pub = rospy.Publisher(f'/{self.bot_name}/detected_tags', Int32MultiArray, queue_size=10)
+        self.tag_info_pub = rospy.Publisher(f'/{self.bot_name}/tag_info', TagInfo, queue_size=10)
+        # Define the wheel command message
+        self.wheel_cmd = WheelsCmdStamped()
+        self.waypoints = self.load_waypoints(self.yaml_file)
+
+
+
+        rospy.on_shutdown(self.shutdown_duckie)
+
+        rospy.loginfo("AprilTag Detector Node initialized.")
+
+    def load_waypoints(self, yaml_file):
+        with open(yaml_file, 'r') as file:
+            data = yaml.safe_load(file)
+        return data['standalone_tags']
+
+    def calib_callback(self, msg):
+        if self.camera_matrix is None:
+            self.camera_matrix = np.array(msg.K).reshape((3, 3))
+            self.dist_coeffs = np.array(msg.D)
+            self.new_camera_matrix, _ = cv2.getOptimalNewCameraMatrix(self.camera_matrix, self.dist_coeffs, (msg.width, msg.height), 1, (msg.width, msg.height))
+            self.map1, self.map2 = cv2.initUndistortRectifyMap(self.camera_matrix, self.dist_coeffs, None, self.new_camera_matrix, (msg.width, msg.height), 5)
+            rospy.loginfo("Camera calibration parameters received.")
+
+    def image_callback(self, msg):
+        
+        if self.map1 is None or self.map2 is None:
+            rospy.logwarn("Camera calibration parameters not yet received.")
+            return
+        
+        # Convert the image from ROS format to OpenCV format
+        image = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="mono8")
+        
+        # Rectify the image
+        rectified_image = cv2.remap(image, self.map1, self.map2, cv2.INTER_LINEAR)
+        
+        # Convert the rectified image back to ROS format
+        rect_img = self.bridge.cv2_to_imgmsg(rectified_image, encoding="mono8")
+        rect_img.header = msg.header
+        self.rect_pub.publish(rect_img)
+
+    def tag_callback(self, msg):
+        
+        # Process the AprilTag detections
+        for detection in msg.detections:
+
+            apriltag_transform = PyQuaternion(detection.pose.pose.pose.orientation.w, detection.pose.pose.pose.orientation.x, detection.pose.pose.pose.orientation.y, detection.pose.pose.pose.orientation.z)
+            
+            tag_id = detection.id[0]
+            matching_waypoint = None
+            for waypoint in self.waypoints:
+                if waypoint['id'] == tag_id:
+                    matching_waypoint = waypoint
+                    break
+
+            # this part needs some comments, some stuff was only found by trial and error
+            if matching_waypoint:
+                #rospy.loginfo(f"Detected tag ID: {tag_id}, matching waypoint: {matching_waypoint['name']}")
+
+                print('got towards publishing')
+                quaternion_q = detection.pose.pose.pose.orientation
+                roll,pitch,yaw = euler_from_quaternion([quaternion_q.x,quaternion_q.y,quaternion_q.z,quaternion_q.w])
+                #rospy.loginfo(f"Yaw: {np.rad2deg(yaw)}, Pitch: {np.rad2deg(pitch)}, Roll: {np.rad2deg(roll)}")
+                apriltag_positioning_quat = PyQuaternion(matching_waypoint['orientation'][3], matching_waypoint['orientation'][0], matching_waypoint['orientation'][1], matching_waypoint['orientation'][2])
+                reference_quat = PyQuaternion(1, 0, 0, 0)
+                rotation_difference = apriltag_positioning_quat * reference_quat.inverse
+
+                yaw = -rotation_difference.yaw_pitch_roll[0] + np.pi
+                #rospy.loginfo(f"marker pos in robo frame {detection.pose.pose.pose.position.x,detection.pose.pose.pose.position.y,detection.pose.pose.pose.position.z}")
+                yaw_quat = PyQuaternion(axis=[0, 0, 1], angle=-yaw)
+                tag_info = TagInfo()
+                tag_info.tag_id = tag_id
+                tag_info.x = detection.pose.pose.pose.position.x
+                tag_info.y = detection.pose.pose.pose.position.y
+                tag_info.z = detection.pose.pose.pose.position.z
+                tag_info.angle = np.rad2deg(pitch)
+                self.tag_info_pub.publish(tag_info)
+
+
+    
+    
+
+    def load_parameters(self):
+        # Load controller configuration
+        drive_controller_file = rospy.get_param('~drive_controller_file', '/code/catkin_ws/src/user_code/quack-norris/params/drive_controller.yaml')
+        self.drive_conroller_config = self.load_yaml_file(drive_controller_file)
+
+        # apriltag_data_file = rospy.get_param('~apriltag_data_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltag_data.yaml')
+        # self.apriltag_data = {}
+        # for tag in self.load_yaml_file(apriltag_data_file):
+        #     self.apriltag_data[tag['id']] = [tag['position'], tag['command']]
+
+        apriltag_data_file = rospy.get_param('~apriltag_data_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltags.yaml')
+        data = self.load_yaml_file(apriltag_data_file)
+        self.apriltag_data = {}
+        for tag in data.get('standalone_tags', []):
+            self.apriltag_data[tag['id']] = [tag['name'], tag['position'], tag['orientation']]
+
+        rospy.loginfo("Configuration parameters loaded.")
+
+    def load_yaml_file(self, file_path):
+        with open(file_path, 'r') as file:
+            return yaml.safe_load(file)
+
+    def drive_forward(self):
+        self.wheel_cmd.vel_left = self.forward_speed 
+        self.wheel_cmd.vel_right = self.forward_speed
+        self.pub_wheels.publish(self.wheel_cmd)
+    
+    def stop_robot(self):
+        self.wheel_cmd.vel_left = 0
+        self.wheel_cmd.vel_right = 0
+        self.pub_wheels.publish(self.wheel_cmd)
+
+    def drive_backward(self):
+        self.wheel_cmd.vel_left = -self.forward_speed #* (1 - self.wheel_distance / (2 * self.radius))
+        self.wheel_cmd.vel_right = -self.forward_speed  #* (1 + self.wheel_distance / (2 * self.radius))
+        self.pub_wheels.publish(self.wheel_cmd)
+
+    def shutdown_duckie(self):
+        rospy.loginfo("Shutting down... stopping the robot.")
+        self.pub_wheels.publish(WheelsCmdStamped())
+        rospy.sleep(1)  # Allow time for the message to propagate
+    
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        node = LocalMapNode()
+        rospy.sleep(1)
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/local_planner.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+import cv2
+from duckietown_msgs.msg import WheelsCmdStamped
+from geometry_msgs.msg import Twist
+from quack_norris.msg import TagInfo
+from sensor_msgs.msg import Image
+import os
+import yaml
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+import matplotlib.pyplot as plt
+import io
+import shapely
+from PIL import Image as PILImage
+from cv_bridge import CvBridge 
+
+
+
+class LocalPlannerNode:
+    def __init__(self):
+        #variables 
+        self.load_parameters()
+
+        self.bot_name = os.environ.get("VEHICLE_NAME")
+
+        self.yaml_file = rospy.get_param('~yaml_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltags.yaml')
+        tag_yaml_file = rospy.get_param("~tag_map_file", "/code/catkin_ws/src/user_code/quack-norris/params/tag_map.yaml")
+        self.tag_map_raw = self.load_yaml_file(tag_yaml_file)['tags']
+
+
+        self.bridge = CvBridge()
+        #vars_for plot
+        self.segment = None
+        self.path = None
+        self.tag_position = None
+        self.tag_orientation = None
+        #subscribers
+        self.tag_info_sub = rospy.Subscriber(f'/{self.bot_name}/tag_info', TagInfo, self.tag_info_cb, queue_size=1)
+
+        #publishers
+        self.image_pub = rospy.Publisher(f'/{self.bot_name}/map_segment', Image, queue_size=1)
+    def load_yaml_file(self, file_path):
+        with open(file_path, 'r') as file:
+            return yaml.safe_load(file)
+    def get_map_parameters(self, tag_id):
+        for tag in self.tag_map_raw:
+            if tag["tag_id"] == tag_id:
+                return tag['map_segment'], tag['path']
+        return None, None
+    
+    def tag_info_cb(self, data):
+        tag_id = data.tag_id
+        self.tag_position = [data.x, data.y,data.z]
+        self.tag_orientation = np.deg2rad(-data.angle)
+
+        self.segment, self.path = self.get_map_parameters(tag_id)
+        self.shapely_path =[ shapely.geometry.LineString(self.path[0]), shapely.geometry.LineString(self.path[1])]
+        self.shapely_path = self.apply_transform(self.shapely_path, self.tag_position, self.tag_orientation)
+
+
+        if self.segment is None:
+            rospy.logwarn(f"Tag {tag_id} not found in the map")
+            
+            return
+        self.plot_and_publish(tag_id)
+        rospy.loginfo(f"Tag {tag_id} found in segment {self.segment} and path {self.path}")
+
+    def apply_transform(self,lines, translation, rotation_angle):
+        transformed_lines = []
+        for line in lines:
+            # Apply translation
+            translated_line = shapely.affinity.translate(line, xoff=translation[0], yoff=translation[1])
+            # Apply rotation
+            rotated_line = shapely.affinity.rotate(translated_line, rotation_angle, origin=(0, 0), use_radians=True)
+            transformed_lines.append(rotated_line)
+        return transformed_lines
+    
+
+    
+    def plot_and_publish(self,tag_id):
+        # Create a plot
+        fig, ax = plt.subplots(figsize=(5, 5))
+        ax.axis('equal')
+        ax.set_xlim(-3, 3)
+        ax.set_ylim(-3, 3)
+        for line in self.shapely_path:
+            x, y = line.xy
+            ax.plot(x, y, label=f"Path {self.segment}")
+        ax.plot(self.tag_position[0], self.tag_position[2], marker='o', label=f"Tag {tag_id} Position", color='red')
+        ax.set_title(f"Map Segment{self.segment} for Tag {tag_id}")
+        ax.set_xlabel("X Coordinate")
+        ax.set_ylabel("Y Coordinate")
+        ax.legend()
+        ax.grid(True)
+        plt.tight_layout()
+
+        # Save the plot to a numpy array
+        buf = io.BytesIO()
+        plt.savefig(buf, format="png")
+        buf.seek(0)
+        pil_img = PILImage.open(buf)
+        cv_img = np.array(pil_img)  # Convert PIL image to numpy array
+        buf.close()
+        plt.close(fig)
+
+        # Publish the plot as a ROS image
+        ros_image = self.bridge.cv2_to_imgmsg(cv_img, encoding="rgba8")
+        self.image_pub.publish(ros_image)
+        rospy.loginfo(f"Published map segment plot for Tag ID {tag_id}")
+
+    def load_parameters(self):
+        # Load controller configuration
+        drive_controller_file = rospy.get_param('~drive_controller_file', '/code/catkin_ws/src/user_code/quack-norris/params/drive_controller.yaml')
+        self.drive_conroller_config = self.load_yaml_file(drive_controller_file)
+
+        # apriltag_data_file = rospy.get_param('~apriltag_data_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltag_data.yaml')
+        # self.apriltag_data = {}
+        # for tag in self.load_yaml_file(apriltag_data_file):
+        #     self.apriltag_data[tag['id']] = [tag['position'], tag['command']]
+
+        apriltag_data_file = rospy.get_param('~apriltag_data_file', '/code/catkin_ws/src/user_code/quack-norris/params/apriltags.yaml')
+        data = self.load_yaml_file(apriltag_data_file)
+        self.apriltag_data = {}
+        for tag in data.get('standalone_tags', []):
+            self.apriltag_data[tag['id']] = [tag['name'], tag['position'], tag['orientation']]
+
+        rospy.loginfo("Configuration parameters loaded.")
+
+    def controller(self):
+        rate = rospy.Rate(10)
+        rospy.wait_for_message(f'/{self.bot_name}/tag_info', TagInfo)
+        while not rospy.is_shutdown():
+            # Implement your controller here
+            rospy.loginfo("Controller running")
+            rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('local_planner_node', anonymous=False)
+    node = LocalPlannerNode()
+    node.controller()
